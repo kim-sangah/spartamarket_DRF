f4f6140bcc89b734507b79138e4deed7cf8456d8
kim-sangah <kimsangah_@naver.com>
2024-09-10 14:40:03 +0900

    feat : add login error
    로그인 실패시 에러 메세지 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 from . import views
 
 
 app_name = "accounts"
 urlpatterns = [
     path('', views.AccountCreate.as_view(), name='signup'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # refresh 토큰도 같이 만듬
+    path('login/', views.Login.as_view(), name='login'),
     path('<str:username>/', views.Profile.as_view() , name='profile'),
     # username이 login인 경우가 생긴다면 다 프로필로 가기에 'profile/<str:username>/'으로 수정이 필요!
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import get_object_or_404
 from .models import Account
 from .serializers import AccountSerializer, ProfileSerializer
+from rest_framework import status
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 
 
 class AccountCreate(generics.CreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
 
-#class CreateAPIView(mixins.CreateModelMixin,
-#                    GenericAPIView):
-#    """
-#    Concrete view for creating a model instance.
-#    """
+#class CreateAPIView(mixins.CreateModelMixin,GenericAPIView):
 #    def post(self, request, *args, **kwargs):
 #        return self.create(request, *args, **kwargs)
+# generics 내 CreateAPIView가 객체를 알아서 저장
+
+
+class Login(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except:
+            return Response(
+                {"로그인 실패: 아이디와 비밀번호를 확인해주세요."},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
 
 class Profile(APIView):

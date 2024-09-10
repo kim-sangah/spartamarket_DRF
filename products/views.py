41f0aa74769bdaf6991a02743ac745273cb3f0c3
kim-sangah <kimsangah_@naver.com>
2024-09-10 13:24:08 +0900

    feat : pagination add
    상품 목록 페이지 보기 추가

--- products/views.py
+++ products/views.py
@@ -5,19 +5,26 @@ from .serializers import ProductSerializer
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from rest_framework.pagination import PageNumberPagination
+
+
+class DefultPagination(PageNumberPagination):
+    page_size = 10
 
 
 class ProductAPIView(APIView):
     # 물건 글 목록
     def get(self, request):
         products = Product.objects.all()
-        # paginator = Paginator(products, 10)  
-        # page_number = request.GET.get("page")
-        # page_obj = paginator.get_page(page_number)
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        paginator = DefultPagination()
+        paginated_product = paginator.paginate_queryset(products, request)
+        serializer = ProductSerializer(paginated_product, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     # 물건 글 작성 (물건 인스턴스 생성)
+    @permission_classes([IsAuthenticated])
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -39,6 +46,9 @@ class ProductDetailAPIView(APIView):
     # 물건 상세 수정
     def put(self, request, productId):
         products = self.get_object(productId)
+        if products.seller != request.user:
+            data = {"message": f"작성자만 수정이 가능합니다"}
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
         serializer = ProductSerializer(products, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
